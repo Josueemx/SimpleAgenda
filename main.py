81d1ef2de751df1105236a3ccaa4f7f0faef2f94
Guadalupe De la Cruz <35407319+FaylinnShinigami@users.noreply.github.com>
2018-01-20 23:07:41 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 from flask import Flask, render_template, request, redirect, session
 import re, sys
+from flaskext.mysql import MySQL
+
+mysql = MySQL()
 
 app = Flask(__name__)
 
+app.config['MYSQL_DATABASE_HOST'] = 'sql3.freemysqlhosting.net'
+app.config['MYSQL_DATABASE_USER'] = 'sql3216877'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'QwFHXgwK62'
+app.config['MYSQL_DATABASE_DB'] = 'sql3216877'
+app.config['MYSQL_DATABASE_PORT'] = 3306
+mysql.init_app(app)
+
 @app.route("/")
 def index():
-    return render_template('index.html')
+    cur = mysql.get_db().cursor()
+    cur.execute('''SELECT * FROM agenda''')
+    rv = cur.fetchall()
+    cur.close()
+    return render_template('index.html', entries=rv)
 
 @app.route("/add", methods = ['POST'])
 def add():
@@ -23,11 +37,15 @@ def add():
         session['errors'] = errors
 
         if len(errors) == 0:
-            n = 1
+            #n = 1
             #aqui lo agrega a la base de datos
+            cur = mysql.get_db().cursor()
+            cur.execute('''INSERT INTO agenda(name,lastname,email,phone,phone_type,address) VALUES(%s,%s,%s,%s,%s,%s)''', (name,lastname,email,phone,phone_type,address))
+            
             
     except:
         errors.append("Something happened in server :(")
+    cur.close()
     return redirect("/", code=302)
 
 @app.route("/update", methods = ['POST'])
@@ -49,11 +67,17 @@ def update():
         if len(errors) == 0:
             n = 1
             #aqui le hace update a la base de datos
+
             
     except:
         errors.append("Something happened in server : "+sys.exc_info()[0])
         
     return redirect("/", code=302)
+
+@app.route("/delete")
+def delete():
+    id_user = int(request.args.get("id","1"))
+    return str(id_user)
 
 def validate(name, email, phone):
     errors = []
